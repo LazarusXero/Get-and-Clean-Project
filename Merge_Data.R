8bc1a1dc52b710ca02530b41a7a8d2d2736366d0
David Leonard <DavidPLeonard@comcast.net>
2015-01-18 10:15:45 -0600

    initial commit of code

---
+++ Merge_Data.R
@@ -0,0 +1,17 @@
+merge_data <- function(){
+        # move working directory to folder 'Data'
+        getwd("Data")
+        
+        
+        # read in test data from 'x_test.txt'
+        # read in test activity numbers from 'y_test.txt'
+        # read in test subject numbers from 'subject_test.txt'
+        
+        # read in training data from 'x_train.txt'
+        # read in training activity numbers from 'y_train.txt'
+        # read in training subject numbers from 'subject_train.txt'
+        
+        # read in activity labeles from 'activity_labels.txt'
+        
+        
+}
