02add78e9b66e7f500b2725a0da7281bede2dee5
David Leonard <DavidPLeonard@comcast.net>
2015-01-18 19:16:53 -0600

    finished part 1 of the project

--- Merge_Data.R
+++ Merge_Data.R
@@ -16,12 +16,14 @@ merge_data <- function(){
         # read in activity labeles from 'activity_labels.txt'
         activity.labels <- read.table("activity_labels.txt")
         
+        # read in feature labels from 'features.txt'
+        feature.data <- read.table("features.txt")
+        
         # merges the activity name in the activity number table in column 2
         for(i in 1:nrow(test.activity.numbers)){
                 test.activity.numbers[i,2] <- activity.labels[test.activity.numbers[i,1],2]
         }
-        
-        
+                
         # read in training data from 'x_train.txt'
         train.data <- read.table("x_train.txt")
         
@@ -36,10 +38,29 @@ merge_data <- function(){
                 train.activity.numbers[i,2] <- activity.labels[train.activity.numbers[i,1],2]
         }
         
+        # column bind test and training activity names to test and training data
+        test.data <- cbind(test.activity.numbers[,2], test.data)
+        train.data <- cbind(train.activity.numbers[,2], train.data)
         
+        # column bind test and training subject numbers to test and training data
+        test.data <- cbind(test.subject.number, test.data)
+        train.data <- cbind(train.subject.number, train.data)
         
+        # match column names and row bind the test and training data together into all.data
+        colnames(test.data) <- colnames(train.data)
+                
+        all.data <- rbind(test.data, train.data, deparse.level = 0)
+        
+        # save feature labels to character vector with two additional columns
+        # for subject number and activity name
+        features <- c("subject.number", "activity.name", as.character(feature.data[,2]))
+        
+        # add column names to all.data table
+        colnames(all.data) <- features
+        
+        ############### end of part 1 from the course project #######################
+                
         # actions to check code above
-        print(unique(train.activity.numbers))
-        
+        all.data.g <<- all.data
         
 }
